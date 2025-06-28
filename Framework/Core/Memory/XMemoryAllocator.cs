7dfa29cf29330307a0941887308595e55657ab27
Dimitri Wang <54432076+DimitriWang@users.noreply.github.com>
2025-06-28 16:46:29 +0800

    [补充 内存申请接口数据]

--- Framework/Core/Memory/XMemoryAllocator.cs
+++ Framework/Core/Memory/XMemoryAllocator.cs
@@ -10,6 +10,19 @@ namespace XIFramework
         {
             GlobalList.Clear();
         }
+        
+        public static void PrintMemCache(Action<string> logHandler)
+        {
+            var output = new System.Text.StringBuilder();
+            foreach (var allocator in XIMemoryAllocatorManager.GlobalList)
+            {
+                if (allocator.FreeCount + allocator.UsedCount > 0)
+                {
+                    output.AppendLine($"{allocator.Name}<{allocator.Type}> UseCount:{allocator.UsedCount}/FreeCount:{allocator.FreeCount}");
+                }
+            }
+            logHandler?.Invoke(output.ToString());
+        }
     }
     public interface IXIMemoryAllocator
     {
@@ -60,5 +73,57 @@ namespace XIFramework
             result = AllocMethod();
             return false;
         }
+
+        public T Alloc()
+        {
+            _usedCount++;
+            if (_freeList.Count > 0)
+            {
+                var result = _freeList[_freeList.Count - 1];
+                _freeList.RemoveAt(_freeList.Count - 1);
+                ResetMethod?.Invoke(result);
+                return result;
+            }
+            return AllocMethod();
+        }
+
+        public void Recycle(T obj)
+        {
+            RecycleMethod?.Invoke(obj);
+            if (_freeList.Contains(obj)) return;
+            _usedCount--;
+            if (_usedCount < 0) _usedCount = 0;
+            _freeList.Add(obj);
+        }
+
+        public void FreeList(List<T> list, bool clear)
+        {
+            foreach (var node in list)
+            {
+                RecycleMethod?.Invoke(node);
+            }
+            _freeList.AddRange(list);
+            _usedCount -= list.Count;
+            if (clear) list.Clear();
+        }
+        
+        public void Clear(Action<T> clearAction = null)
+        {
+            if (clearAction != null)
+            {
+                foreach (var item in _freeList)
+                {
+                    clearAction(item);
+                }
+            }
+            _freeList.Clear();
+        }
+        
+        // 新增静态打印方法
+        public static void PrintMemCache(Action<string> logHandler)
+        {
+            XIMemoryAllocatorManager.PrintMemCache(logHandler);
+        }
+
     }
 }
